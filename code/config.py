f20d974d1d517ec1c5d60307587490c29ed1ba46
Friedrich Geiecke <f.c.geiecke@lse.ac.uk>
2025-02-21 19:51:32 +0000

    update library versions

--- code/config.py
+++ code/config.py
@@ -76,7 +76,7 @@ SYSTEM_PROMPT = f"""{INTERVIEW_OUTLINE}
 # API parameters
 MODEL = "gpt-4o-2024-05-13"  # or e.g. "claude-3-5-sonnet-20240620" (OpenAI GPT or Anthropic Claude models)
 TEMPERATURE = None  # (None for default value)
-MAX_OUTPUT_TOKENS = 1024
+MAX_OUTPUT_TOKENS = 2048
 
 
 # Display login screen with usernames and simple passwords for studies
--- code/interview.py
+++ code/interview.py
@@ -106,10 +106,10 @@ for message in st.session_state.messages[1:]:
 
 # Load API client
 if api == "openai":
-    client = OpenAI(api_key=st.secrets["API_KEY"])
+    client = OpenAI(api_key=st.secrets["API_KEY_OPENAI"])
     api_kwargs = {"stream": True}
 elif api == "anthropic":
-    client = anthropic.Anthropic(api_key=st.secrets["API_KEY"])
+    client = anthropic.Anthropic(api_key=st.secrets["API_KEY_ANTHROPIC"])
     api_kwargs = {"system": config.SYSTEM_PROMPT}
 
 # API kwargs
